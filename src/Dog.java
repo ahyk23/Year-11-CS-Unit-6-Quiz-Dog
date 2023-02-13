2147cd87ef5bcba499238e590784ca1e131f5fe4
alaynakwan <ahyk23@student.cis.edu.hk>
2023-02-13 12:38:59 +0800

    Dog class initialized

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,45 @@
 public class Dog {
+    private int age;
+    private String name;
+    private String color;
+
+    public Dog(){
+        age = 0;
+        name = "";
+        color = "";
+    }
+    public Dog(int a, String n, String c){
+        age = a;
+        name = n;
+        color = c;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setAge(int a){
+        age = a;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public void setColor(String c){
+        color = c;
+    }
+    private void addOneAge(){
+        age += 1;
+    }
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+    public String toString(){
+        return "The dog is named " + name + " and is " + age + " years old. This dog is " + color + ".";
+    }
 }
